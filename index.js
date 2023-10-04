ad3cb212e7bb7c8d29916e60b94bac7818590285
Isaiah <IaguileraCFE@gmail.com>
2023-10-03 19:15:42 -0700

    Isaiah A Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const cards = ["Charlie", "Samip", "Ali"];
+
+function writeCards(cards){
+    const messages = []
+    for(let i =0; i<cards.length; i++){
+        console.log(`Thank you, ${cards[i]} for the wonderful birthday gift!`);
+        messages.push (`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    }
+    return messages;
+}
+writeCards(cards)
+
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
